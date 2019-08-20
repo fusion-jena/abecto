729126d01942ae757cc8ab1bd72267f2ac79bff7
Jan Martin Keil <jan-martin.keil@uni-jena.de>
2019-08-20 10:44:04 +0200

    remove needles @Transaction

--- src/main/java/de/uni_jena/cs/fusion/abecto/knowledgebase/KnowledgeBaseRestController.java
+++ src/main/java/de/uni_jena/cs/fusion/abecto/knowledgebase/KnowledgeBaseRestController.java
@@ -5,7 +5,6 @@ import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,7 +18,6 @@ import de.uni_jena.cs.fusion.abecto.project.Project;
 import de.uni_jena.cs.fusion.abecto.project.ProjectRepository;
 
 @RestController
-@Transactional
 public class KnowledgeBaseRestController {
 
 	@Autowired
