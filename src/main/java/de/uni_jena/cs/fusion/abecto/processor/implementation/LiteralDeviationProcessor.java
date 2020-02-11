a8add054752d6a4b71e330c17eb69fb367a64c3f
Jan Martin Keil <jan-martin.keil@uni-jena.de>
2020-02-11 16:17:36 +0100

    first draft LiteralDeviationProcessor (untested)

---
+++ src/main/java/de/uni_jena/cs/fusion/abecto/processor/implementation/LiteralDeviationProcessor.java
@@ -0,0 +1,136 @@
+package de.uni_jena.cs.fusion.abecto.processor.implementation;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+import java.util.HashSet;
+import java.util.UUID;
+
+import org.apache.jena.query.QuerySolution;
+import org.apache.jena.query.ResultSet;
+import org.apache.jena.rdf.model.Literal;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.Resource;
+
+import de.uni_jena.cs.fusion.abecto.parameter_model.ParameterModel;
+import de.uni_jena.cs.fusion.abecto.processor.AbstractMetaProcessor;
+import de.uni_jena.cs.fusion.abecto.processor.model.Category;
+import de.uni_jena.cs.fusion.abecto.processor.model.Issue;
+import de.uni_jena.cs.fusion.abecto.processor.model.Mapping;
+import de.uni_jena.cs.fusion.abecto.processor.model.ValueDeviation;
+import de.uni_jena.cs.fusion.abecto.sparq.SparqlEntityManager;
+
+public class LiteralDeviationProcessor extends AbstractMetaProcessor<LiteralDeviationProcessor.Parameter> {
+
+	public class Parameter implements ParameterModel {
+		/**
+		 * Name of variables by name of categories to process.
+		 */
+		Map<String, Collection<String>> variables;
+	}
+
+	@Override
+	protected void computeResultModel() throws Exception {
+		Set<UUID> knowledgeBaseIds = this.inputGroupModels.keySet();
+
+		Collection<ValueDeviation> deviations = new ArrayList<>();
+
+		// iterate knowledge base pairs
+		for (UUID knowledgeBaseId1 : knowledgeBaseIds) {
+			Model model1 = this.inputGroupModels.get(knowledgeBaseId1);
+			for (UUID knowledgeBaseId2 : knowledgeBaseIds) {
+				Model model2 = this.inputGroupModels.get(knowledgeBaseId2);
+				if (knowledgeBaseId1.compareTo(knowledgeBaseId2) > 0) {
+					// iterate categories
+					for (String categoryName : this.getParameters().variables.keySet()) {
+						Optional<Category> category1Optional = SparqlEntityManager
+								.selectOne(new Category(categoryName, null, knowledgeBaseId1), this.metaModel);
+						Optional<Category> category2Optional = SparqlEntityManager
+								.selectOne(new Category(categoryName, null, knowledgeBaseId2), this.metaModel);
+						if (category1Optional.isPresent() && category2Optional.isPresent()) {
+							Category category1 = category1Optional.orElseThrow();
+							Category category2 = category2Optional.orElseThrow();
+
+							// load mapping
+							Map<Resource, Set<Resource>> mappings = new HashMap<>();
+							for (Mapping mapping : SparqlEntityManager.select(
+									Arrays.asList(Mapping.of(knowledgeBaseId1, knowledgeBaseId2, categoryName),
+											Mapping.of(knowledgeBaseId2, knowledgeBaseId1, categoryName)),
+									this.metaModel)) {
+								mappings.computeIfAbsent(mapping.getResourceOf(knowledgeBaseId1), (x) -> {
+									return new HashSet<>();
+								}).add(mapping.getResourceOf(knowledgeBaseId2));
+							}
+
+							Collection<String> variableNames = this.getParameters().variables.get(categoryName);
+
+							Map<Resource, Map<String, Literal>> valuesByVariableByResource2 = new HashMap<>();
+							ResultSet results2 = category2.selectCategory(model2);
+							while (results2.hasNext()) {
+								QuerySolution result2 = results2.next();
+								Resource resoure2 = result2.getResource(categoryName);
+
+								Map<String, Literal> valuesByVariable2 = valuesByVariableByResource2
+										.computeIfAbsent(resoure2, (x) -> {
+											return new HashMap<>();
+										});
+								// iterate variables
+								for (String variableName : variableNames) {
+									if (result2.contains(variableName)) {
+										try {
+											valuesByVariable2.put(variableName, result2.getLiteral(variableName));
+										} catch (ClassCastException e) {
+											Issue issue = new Issue(null, knowledgeBaseId2, resoure2,
+													"UnexpectedValueType",
+													String.format("Value of property %s is not a literal.",
+															variableName, resoure2.getURI()));
+											SparqlEntityManager.insert(issue, this.getResultModel());
+										}
+									}
+								}
+							}
+
+							ResultSet results1 = category1.selectCategory(model1);
+							while (results1.hasNext()) {
+								QuerySolution result1 = results1.next();
+								Resource resource1 = result1.getResource(categoryName);
+
+								// iterate variables
+								for (String variableName : variableNames) {
+									if (result1.contains(variableName)) {
+										try {
+											Literal value1 = result1.getLiteral(variableName);
+											for (Resource resource2 : mappings.get(resource1)) {
+												Literal value2 = valuesByVariableByResource2.get(resource2)
+														.get(variableName);
+												if (!value1.sameValueAs(value2)) {
+													deviations.add(new ValueDeviation(null, categoryName, variableName,
+															resource1, resource2, knowledgeBaseId1, knowledgeBaseId2,
+															value1.getLexicalForm(), value2.getLexicalForm()));
+												}
+											}
+										} catch (ClassCastException e) {
+											Issue issue = new Issue(null, knowledgeBaseId1, resource1,
+													"UnexpectedValueType",
+													String.format("Value of property %s is not a literal.",
+															variableName, resource1.getURI()));
+											SparqlEntityManager.insert(issue, this.getResultModel());
+										}
+
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+
+		SparqlEntityManager.insert(deviations, this.getResultModel());
+	}
+
+}
---
+++ src/main/java/de/uni_jena/cs/fusion/abecto/processor/model/ValueDeviation.java
@@ -0,0 +1,78 @@
+package de.uni_jena.cs.fusion.abecto.processor.model;
+
+import java.util.UUID;
+
+import org.apache.jena.rdf.model.Resource;
+
+import de.uni_jena.cs.fusion.abecto.sparq.Member;
+import de.uni_jena.cs.fusion.abecto.sparq.SparqlNamespace;
+import de.uni_jena.cs.fusion.abecto.sparq.SparqlPattern;
+
+@SparqlNamespace(prefix = "rdf", namespace = "http://www.w3.org/1999/02/22-rdf-syntax-ns#")
+@SparqlNamespace(prefix = "abecto", namespace = "http://fusion.cs.uni-jena.de/ontology/abecto#")
+public class ValueDeviation {
+	@SparqlPattern(predicate = "rdf:type", object = "abecto:ValueDeviation")
+	public Resource id;
+	@SparqlPattern(subject = "id", predicate = "abecto:categoryName")
+	public final String categoryName;
+	@SparqlPattern(subject = "id", predicate = "abecto:variableName")
+	public final String variableName;
+	@SparqlPattern(subject = "id", predicate = "abecto:resource1")
+	public final Resource resource1;
+	@SparqlPattern(subject = "id", predicate = "abecto:resource2")
+	public final Resource resource2;
+	@SparqlPattern(subject = "id", predicate = "abecto:knowledgeBaseId1")
+	public final UUID knowledgeBaseId1;
+	@SparqlPattern(subject = "id", predicate = "abecto:knowledgeBaseId2")
+	public final UUID knowledgeBaseId2;
+	@SparqlPattern(subject = "id", predicate = "abecto:value1")
+	public final String value1;
+	@SparqlPattern(subject = "id", predicate = "abecto:value2")
+	public final String value2;
+
+	public ValueDeviation(@Member("id") Resource id, @Member("categoryName") String categoryName,
+			@Member("variableName") String variableName, @Member("resource1") Resource resource1,
+			@Member("resource2") Resource resource2, @Member("knowledgeBaseId1") UUID knowledgeBaseId1,
+			@Member("knowledgeBaseId2") UUID knowledgeBaseId2, @Member("value1") String value1,
+			@Member("value2") String value2) {
+		this.id = id;
+		this.categoryName = categoryName;
+		this.variableName = variableName;
+		if (resource1 == null || resource2 != null && resource1.getURI().compareTo(resource2.getURI()) < 0) {
+			this.resource1 = resource1;
+			this.resource2 = resource2;
+			this.knowledgeBaseId1 = knowledgeBaseId1;
+			this.knowledgeBaseId2 = knowledgeBaseId2;
+			this.value1 = value1;
+			this.value2 = value2;
+		} else {
+			this.resource1 = resource2;
+			this.resource2 = resource1;
+			this.knowledgeBaseId1 = knowledgeBaseId2;
+			this.knowledgeBaseId2 = knowledgeBaseId1;
+			this.value1 = value2;
+			this.value2 = value1;
+		}
+	}
+
+	public Resource getResourceOf(UUID knowledgeBase) {
+		if (this.knowledgeBaseId1 != null && knowledgeBase.equals(this.knowledgeBaseId1)) {
+			return this.resource1;
+		} else if (this.knowledgeBaseId2 != null && knowledgeBase.equals(this.knowledgeBaseId2)) {
+			return this.resource2;
+		} else {
+			throw new IllegalArgumentException("Unknown knowledge base.");
+		}
+	}
+
+	public String getValueOf(UUID knowledgeBase) {
+		if (this.knowledgeBaseId1 != null && knowledgeBase.equals(this.knowledgeBaseId1)) {
+			return this.value1;
+		} else if (this.knowledgeBaseId2 != null && knowledgeBase.equals(this.knowledgeBaseId2)) {
+			return this.value2;
+		} else {
+			throw new IllegalArgumentException("Unknown knowledge base.");
+		}
+	}
+
+}
